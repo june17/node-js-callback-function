9bcf208f74d528234cfc46fb02b477da483fb015
Arunraj <arunraj.17j@gmail.com>
2014-08-09 14:14:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 
-var server =http.createServer(function(request,res){
-	getTitles(res);
+var server =http.createServer(function(request,res){ //Client request initially comes here
+	getTitles(res); // Control passed to getTitles
 }).listen(8000);
 console.log("Server running");
 
 function getTitles(res) {
-    fs.readFile('./titles.json', function(err, data) {
+    fs.readFile('./titles.json', function(err, data) { //getTitles pulling data from titles.json
       if (err) {
       	hadError(err,res);
       }
@@ -17,7 +17,7 @@ function getTitles(res) {
 	})
 }
 
-function getTemplate(titles,res){
+function getTemplate(titles,res){ //getTemplate reads template file index.html
         fs.readFile('./index.html', function(err, data) {
           if (err) {
            	hadError(err,res); 
@@ -28,13 +28,13 @@ function getTemplate(titles,res){
     });      
 }
 
-function formatHtml(titles,tmpl,res){
+function formatHtml(titles,tmpl,res){ //formatHtml takes titles and template and render response
         var html = tmpl.replace('%', titles.join('</li><li>'));
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(html);      
 }
 
-function hadError(err,res){
+function hadError(err,res){ //error passed
 	console.error(err);
 	res.end('Something gone wrong, yes! its the server');
 }
